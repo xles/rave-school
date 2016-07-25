aecc887632ce0ed1904205802c13c0e2ca289977
xles McFuzzypants <xles@mirakulix.org>
2016-07-25 03:45:42 +0200

    Create lesson-2.cs

---
+++ lesson-2.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool hello = true;
+
+        if (hello) {
+            Console.WriteLine("Hello, world!");
+        } else {
+            Console.WriteLine("Goodbye, world!");
+        }
+    }
+}
