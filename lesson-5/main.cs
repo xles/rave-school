e6b4a3ed285e40df3e08ceab938e3f49f33171a9
xles <xles@mirakulix.org>
2016-07-25 04:29:28 +0200

    Added lesson 5

---
+++ lesson-5/main.cs
@@ -0,0 +1,35 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Hello();
+    }
+
+    static void Hello()
+    {
+        Console.WriteLine("Hello, world...again!");    
+    }
+}
+
+/*
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x;
+        x = Increment(x);
+        Console.WriteLine(x);
+    }
+
+    static int Increment(int input)
+    {
+        int result;
+        result = input + 1;
+        return result;
+    }
+}
+*/
